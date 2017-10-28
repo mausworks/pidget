7254b889ffd8614b2a2131defd9a1b5619c4a6e9
mausworks <me@fixz.se>
2017-10-28 23:27:08 +0200

    Added "manual test" for sending events to prod.

---
+++ test/Raven.Client.Test/Http/RavenHttpClientTests.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Raven.Client.Http;
+using Xunit;
+
+namespace Raven.Client.Test
+{
+    public class RavenHttpClientTests
+    {
+        public static readonly Dsn Dsn = Dsn.Create(GetDsn());
+
+        [Fact(Skip = "Manual testing only.")]
+        public async Task SendException_ReturnsEventId()
+        {
+            var client = Sentry.GetRavenClient(Dsn);
+
+            var value = 0;
+
+            try
+            {
+                var x = 10 / value;
+            }
+            catch (Exception ex)
+            {
+                var id = await client.CaptureAsync(e => e
+                    .SetException(ex)
+                    .SetErrorLevel(ErrorLevel.Warning)
+                    .AddExtraData("test", new
+                    {
+                        IsTest = true
+                    })
+                    .AddTag("test_tag", "yes"));
+
+                Assert.NotNull(id);
+            }
+        }
+
+        private static string GetDsn()
+        {
+            var cwd = Directory.GetCurrentDirectory();
+
+            return File.ReadAllText(Path.Combine(cwd, "dsn.txt")).Trim();
+        }
+    }
+}
