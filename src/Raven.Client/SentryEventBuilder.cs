e873948d132feb2ef910ebd849a8cf94319c7950
mausworks <me@fixz.se>
2017-10-28 22:59:23 +0200

    Created a sentry event builder.

---
+++ src/Raven.Client/SentryEventBuilder.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Raven.Client
+{
+    /// <summary>
+    /// Provides methods for building a sentry event.
+    /// </summary>
+    public class SentryEventBuilder
+    {
+        private Exception _exception;
+
+        private string _message;
+
+        private string _transaction;
+
+        private ErrorLevel _errorLevel;
+
+        private readonly Dictionary<string, string> _tags
+            = new Dictionary<string, string>(4);
+
+        private readonly Dictionary<string, object> _extraData
+            = new Dictionary<string, object>(4);
+
+        private readonly List<string> _fingerprint = new List<string>(4);
+
+        /// <summary>
+        /// Sets the captured exception.
+        /// </summary>
+        /// <param name="exception">The exception to capture.</param>
+        public SentryEventBuilder SetException(Exception exception)
+        {
+            _exception = exception;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Sets an optional message for the event.
+        /// </summary>
+        /// <param name="message">The message to capture.</param>
+        public SentryEventBuilder SetMessage(string message)
+        {
+            _message = message;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Overrides the name of the transaction for the event.
+        /// </summary>
+        /// <param name="transactionName">
+        ///     The name of the transaction to use for the captured event.
+        /// </param>
+        public SentryEventBuilder SetTransaction(string transactionName)
+        {
+            _transaction = transactionName;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Overrides the default error level.
+        /// <para>The default error level is "error".</para>
+        /// </summary>
+        /// <param name="level">
+        ///     The error level to use for the captured event
+        /// </param>
+        public SentryEventBuilder SetErrorLevel(ErrorLevel level)
+        {
+            _errorLevel = level;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Adds a tag with the provided name and value to the event.
+        /// </summary>
+        /// <param name="name">The name of the tag, e.g.; "ios_version".</param>
+        /// <param name="value">The value of the tag, e.g.; "8".</param>
+        public SentryEventBuilder AddTag(string name, string value)
+        {
+            _tags.Add(name, value);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Adds a collection of tags to the event from enumerable key/value-pairs.
+        /// </summary>
+        /// <param name="tags">Enumerable key/value-pairs to add as tags.</param>
+        public SentryEventBuilder AddTags(
+            IEnumerable<KeyValuePair<string, string>> tags)
+        {
+            foreach (var tag in tags)
+            {
+                _tags.Add(tag.Key, tag.Value);
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Adds extra data under the specified key to the event.
+        /// </summary>
+        /// <param name="key">The name of the extra data, e.g.; "request_body".</param>
+        /// <param name="data">The data (gets serialized to JSON).</param>
+        public SentryEventBuilder AddExtraData(string key, object data)
+        {
+            _extraData.Add(key, data);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Adds the provided data to the fingerprint.
+        /// </summary>
+        /// <param name="data">The data, e.g.; "GET", "/index"</param>
+        public SentryEventBuilder AddFingerprintData(params string[] data)
+        {
+            _fingerprint.AddRange(data);
+
+            return this;
+        }
+
+        public SentryEvent Build()
+        {
+            AssertValidity();
+
+            return new SentryEvent(
+                _exception,
+                _errorLevel,
+                _transaction,
+                _message,
+                _extraData,
+                _tags,
+                _fingerprint);
+        }
+
+        private void AssertValidity()
+        {
+            if (_exception == null)
+            {
+                throw new InvalidOperationException(
+                    "An exception must be set.");
+            }
+        }
+    }
+}
---
+++ test/Raven.Client.Test/SentryEventBuilderTests.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Raven.Client.Test
+{
+    public class SentryEventBuilderTests
+    {
+        [Fact]
+        public void SetException()
+        {
+            var exception = new Exception();
+
+            var builder = new SentryEventBuilder()
+                .SetException(exception);
+
+            Assert.Equal(exception, builder.Build().Exception);
+        }
+
+        [Theory, InlineData("transaction")]
+        public void SetTransaction(string transaction)
+        {
+            var builder = new SentryEventBuilder()
+                .SetException(new Exception())
+                .SetTransaction(transaction);
+
+            Assert.Equal(transaction, builder.Build().Transaction);
+        }
+
+        [Theory, InlineData("message")]
+        public void SetMessage(string message)
+        {
+            var builder = new SentryEventBuilder()
+                .SetException(new Exception())
+                .SetMessage(message);
+
+            Assert.Equal(message, builder.Build().Message);
+        }
+
+        [Theory, InlineData(ErrorLevel.Fatal)]
+        public void SetErrorLevel(ErrorLevel level)
+        {
+            var builder = new SentryEventBuilder()
+                .SetException(new Exception())
+                .SetErrorLevel(level);
+
+            Assert.Equal(level, builder.Build().ErrorLevel);
+        }
+
+        [Theory, InlineData("name", "value")]
+        public void AddTag(string name, string value)
+        {
+            var builder = new SentryEventBuilder()
+                .SetException(new Exception())
+                .AddTag(name, value);
+
+            var kvp = builder.Build().Tags.First();
+
+            Assert.Equal(name, kvp.Key);
+            Assert.Equal(value, kvp.Value);
+        }
+
+        [Theory, InlineData("name", "value")]
+        public void AddTags(string name, string value)
+        {
+            var builder = new SentryEventBuilder()
+                .SetException(new Exception())
+                .AddTags(new Dictionary<string, string> { { name, value } });
+
+            var kvp = builder.Build().Tags.First();
+
+            Assert.Equal(name, kvp.Key);
+            Assert.Equal(value, kvp.Value);
+        }
+
+        [Theory, InlineData("extra_data", "foo")]
+        public void AddExtraData(string name, object value)
+        {
+            var builder = new SentryEventBuilder()
+                .SetException(new Exception())
+                .AddExtraData(name, value);
+
+            var extraData = builder.Build().ExtraData;
+
+            Assert.Equal(name, extraData.First().Key);
+            Assert.Equal(value, extraData.First().Value);
+        }
+
+        [Theory, InlineData("foo", "bar")]
+        public void AddFingerprintData(params string[] data)
+        {
+            var builder = new SentryEventBuilder()
+                .SetException(new Exception())
+                .AddFingerprintData(data);
+
+            Assert.Equal(data, builder.Build().Fingerprint);
+        }
+    }
+}
