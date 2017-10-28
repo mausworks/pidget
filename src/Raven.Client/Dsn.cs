6417ce981c988bce142c506068c2b601e98d77a7
mausworks <me@fixz.se>
2017-10-28 22:54:55 +0200

    Created a DSN wrapper.

---
+++ src/Raven.Client/Dsn.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Raven.Client
+{
+    public class Dsn
+    {
+        public Uri Uri { get; }
+
+        private Dsn(Uri uri)
+            => Uri = uri;
+
+        public string GetProjectId()
+            => Uri.AbsoluteUri.Substring(
+                LastIndexOfSlash(Uri.AbsoluteUri) + 1);
+
+        public string GetPath()
+            => Uri.AbsolutePath.Substring(0,
+                LastIndexOfSlash(Uri.AbsolutePath) + 1);
+
+        public string GetPublicKey()
+            => Uri.UserInfo.Split(':')[0];
+
+        public string GetSecretKey()
+            => Uri.UserInfo.Split(':')[1];
+
+        private int LastIndexOfSlash(string input)
+            => input.LastIndexOf('/');
+
+        public override string ToString()
+            => Uri.ToString();
+
+        public static Dsn Create(string dsn)
+        {
+            Assert.ArgumentNotNull(dsn, nameof(dsn));
+
+            return new Dsn(new Uri(dsn));
+        }
+    }
+}
