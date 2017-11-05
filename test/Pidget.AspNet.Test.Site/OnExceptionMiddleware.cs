b9f580019be85439688cd5abb65b711a4a9be125
mausworks <me@fixz.se>
2017-11-05 23:51:44 +0100

    Added a test website.

---
+++ test/Pidget.AspNet.Test.Site/OnExceptionMiddleware.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+namespace Pidget.AspNet.Test.Site
+{
+    public class OnExceptionMiddleware
+    {
+        public readonly string _eventIdKey;
+
+        private readonly RequestDelegate _next;
+
+        public OnExceptionMiddleware(RequestDelegate next,
+            IOptions<ExceptionReportingOptions> options)
+        {
+            _next = next;
+            _eventIdKey = options.Value.EventIdKey;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = 200;
+                context.Response.ContentType = "text/plain";
+
+                await WriteException(context, ex);
+            }
+        }
+
+        public async Task WriteException(HttpContext context, Exception ex)
+        {
+            await context.Response.WriteAsync(
+                $"An exception of the type '{ex.GetType().Name}' occurred\r\n");
+            await context.Response.WriteAsync(
+                $" - Exception.Data[\"{_eventIdKey}\"]: {ex.Data[_eventIdKey]}\r\n");
+            await context.Response.WriteAsync(
+                $" - HttpContext.Items[\"{_eventIdKey}\"]: {context.Items[_eventIdKey]}");
+        }
+    }
+}
---
+++ test/Pidget.AspNet.Test.Site/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Pidget.AspNet.DependencyInjection;
+
+namespace Pidget.AspNet.Test.Site
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+            => Configuration = configuration;
+
+        public void ConfigureServices(IServiceCollection services)
+            => services.ConfigurePidgetMiddleware(
+                Configuration.GetSection("ExceptionReporting"));
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMiddleware<OnExceptionMiddleware>();
+            app.UseMiddleware<ExceptionReportingMiddleware>();
+
+            app.Run(context =>
+            {
+                throw new InvalidOperationException(
+                    "This app is not meant to run!");
+            });
+        }
+    }
+}
