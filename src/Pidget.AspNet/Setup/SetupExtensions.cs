3764995ee2ea9856077dda6fd5dc93ef605c7016
mausworks <me@fixz.se>
2017-11-12 22:52:29 +0100

    Rename Configure => Add

--- src/Pidget.AspNet/Setup/SetupExtensions.cs
+++ src/Pidget.AspNet/Setup/SetupExtensions.cs
@@ -10,12 +10,12 @@ namespace Pidget.AspNet.Setup
 {
     public static class SetupExtensions
     {
-        public static IServiceCollection ConfigurePidgetMiddleware(
+        public static IServiceCollection AddPidgetMiddleware(
             this IServiceCollection services,
             IConfiguration configuration)
-            => ConfigurePidgetMiddleware(services, configuration.Bind);
+            => AddPidgetMiddleware(services, configuration.Bind);
 
-        public static IServiceCollection ConfigurePidgetMiddleware(
+        public static IServiceCollection AddPidgetMiddleware(
             this IServiceCollection services,
             Action<ExceptionReportingOptions> setup)
             => services.Configure<ExceptionReportingOptions>(setup)
