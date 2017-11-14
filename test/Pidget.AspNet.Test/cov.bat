cf52e59fa5c3e4dd5a5cef063feb9512dcb419d5
mausworks <me@fixz.se>
2017-11-14 20:28:54 +0100

    Added code coverage.

---
+++ test/Pidget.AspNet.Test/cov.bat
@@ -0,0 +1,38 @@
+@echo off
+
+:parse
+if "%~1"=="" goto endparse
+if "%~1"=="--no-report" set noreport=1
+if "%~1"=="--no-test" set notest=1
+shift
+goto parse
+:endparse
+
+set project="Pidget.AspNet"
+
+set packages="..\..\tools\code-coverage\.packages\"
+set dotnet="C:\Program Files\dotnet\dotnet.exe"
+set opencover="%packages%\OpenCover\4.6.519\tools\OpenCover.Console.exe"
+set reportgenerator="%packages%\ReportGenerator\3.0.2\tools\ReportGenerator.exe"
+set coveragefile=.cov\coverage.xml
+set coveragedir=.cov\
+
+if not exist %packages% (
+  %dotnet% restore ..\..\tools\code-coverage\code-coverage.csproj --packages ..\..\tools\code-coverage\packages
+)
+
+if "%notest%"=="" %opencover%^
+  -target:%dotnet%^
+  -targetargs:test^
+  -filter:"+[%project%*]* -[*.Test]* -[Xunit.*]* -[Moq.*]*"^
+  -output:%coveragefile%^
+  -register:user^
+  -oldStyle^
+  -skipautoprops^
+  -hideskipped:All
+
+if "%noreport%"=="" %reportgenerator%^
+  -targetdir:%coveragedir%^
+  -reporttypes:Html;Badges^
+  -reports:%coveragefile%^
+  -verbosity:Error & start "Report" "%coveragedir%index.htm"
