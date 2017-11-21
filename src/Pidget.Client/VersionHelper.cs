df3e4113da27bff0680ded98ba1e56d12f06a7ee
mausworks <me@fixz.se>
2017-11-21 21:33:06 +0100

    Cache product version in static field

--- src/Pidget.Client/VersionHelper.cs
+++ src/Pidget.Client/VersionHelper.cs
@@ -5,7 +5,12 @@ namespace Pidget.Client
 {
     internal static class VersionHelper
     {
+        private static string _versionNumber;
+
         public static string GetVersionNumber()
+            => (_versionNumber ?? (_versionNumber = GetProductVersion()));
+
+        private static string GetProductVersion()
             => FileVersionInfo.GetVersionInfo(
                 Assembly.GetExecutingAssembly().Location).ProductVersion;
     }
