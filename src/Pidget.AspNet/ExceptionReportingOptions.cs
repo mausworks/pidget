4a60a21512de7f6479620dab001170dbfb52d1c2
mausworks <rasmuswennerstrom@gmail.com>
2018-02-08 20:17:37 +0100

    Add callbacks to options, add helper class.

--- src/Pidget.AspNet/ExceptionReportingOptions.cs
+++ src/Pidget.AspNet/ExceptionReportingOptions.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Pidget.AspNet.Setup;
+using Pidget.Client;
+
 namespace Pidget.AspNet
 {
     public class ExceptionReportingOptions
@@ -6,5 +12,12 @@ namespace Pidget.AspNet
 
         public SanitationOptions Sanitation { get; set; }
             = SanitationOptions.Default;
+
+        public Func<SentryEventBuilder, HttpContext, Task<bool>> BeforeSendCallback { get; set; }
+            = ((b, h) => Task.FromResult(true));
+        public Func<SentryResponse, HttpContext, Task> AfterSendCallback { get; set; }
+            = ((r, h) => Task.CompletedTask);
+
+        public CallbackSetup Callbacks => new CallbackSetup(this);
     }
 }
---
+++ src/Pidget.AspNet/Setup/CallbackSetup.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Pidget.Client;
+
+namespace Pidget.AspNet.Setup
+{
+    public struct CallbackSetup
+    {
+        private readonly ExceptionReportingOptions _options;
+
+        public CallbackSetup(ExceptionReportingOptions options) : this()
+            => _options = options;
+
+        public CallbackSetup BeforeSend(Func<SentryEventBuilder, HttpContext, Task<bool>> callback)
+        {
+            _options.BeforeSendCallback = callback;
+
+            return this;
+        }
+
+        public CallbackSetup AfterSend(Func<SentryResponse, HttpContext, Task> callback)
+        {
+            _options.AfterSendCallback = callback;
+
+            return this;
+        }
+    }
+}
