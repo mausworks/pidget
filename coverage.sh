9d2fa7dfd545f30a2dbbb040bab65d75be89553a
mausworks <rasmuswennerstrom@gmail.com>
2018-02-09 00:35:37 +0100

    Add minicover

---
+++ coverage.sh
@@ -0,0 +1,28 @@
+dotnet restore
+dotnet build
+
+cd tools/code-coverage
+
+# Instrument assemblies inside 'test' folder to detect hits for source files inside 'src' folder
+dotnet minicover instrument --workdir ../../ --assemblies test/**/bin/**/*.dll --sources src/**/*.cs
+
+# Reset hits count in case minicover was run for this project
+dotnet minicover reset
+
+cd ../../
+
+for project in test/**/*.csproj; do dotnet test --no-build $project; done
+
+cd tools/code-coverage
+
+# Uninstrument assemblies, it's important if you're going to publish or deploy build outputs
+dotnet minicover uninstrument --workdir ../../
+
+# Create html reports inside folder coverage-html
+dotnet minicover htmlreport --workdir ../../ --threshold 90
+
+# Print console report
+# This command returns failure if the coverage is lower than the threshold
+dotnet minicover report --workdir ../../ --threshold 90
+
+cd ..
