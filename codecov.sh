f62f9b4a7848c33f18e0e8a7052722a3c54cb2bd
mausworks <rasmuswennerstrom@gmail.com>
2018-03-02 22:13:32 +0100

    Add script for uploading reports to codecov

---
+++ codecov.sh
@@ -0,0 +1,23 @@
+dotnet restore
+
+cd tools/code-coverage
+
+# Instrument assemblies inside 'test' folder to detect hits for source files inside 'src' folder
+dotnet minicover instrument --workdir ../../ --assemblies test/**/bin/**/*.dll --sources src/**/*.cs
+
+# Reset hits count in case minicover was run for this project
+dotnet minicover reset
+
+cd ../../
+
+for project in test/**/*.csproj; do dotnet test $project; done
+
+cd tools/code-coverage
+
+dotnet minicover uninstrument --workdir ../../
+
+dotnet minicover opencoverreport --workdir ../../ --output coverage.xml
+
+cd ../..
+
+curl -s https://codecov.io/bash | bash -s - -t ac12fe3c-28ae-4b9c-b65c-4d3879e7e942
