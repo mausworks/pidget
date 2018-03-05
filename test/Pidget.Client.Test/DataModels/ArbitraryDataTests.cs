96f20996a94af8dc235184de6d52ecf0dc6d77b8
mausworks <rasmuswennerstrom@gmail.com>
2018-03-05 22:56:53 +0100

    Add tests for dictionary impl.

--- test/Pidget.Client.Test/DataModels/ArbitraryDataTests.cs
+++ test/Pidget.Client.Test/DataModels/ArbitraryDataTests.cs
@@ -36,5 +36,31 @@ namespace Pidget.Client.Test.DataModels
             Assert.True(data.TryGetValue(key, out object outValue));
             Assert.Equal(value, outValue);
         }
+
+        [Fact]
+        public void ImplementsDictionary()
+        {
+            var data = new ArbitraryData()
+            {
+                { "foo", "bar" }
+            };
+
+            data.Add("bar", "baz");
+            data.Add(new KeyValuePair<string, object>("baz", "foo"));
+
+            Assert.False(data.IsReadOnly);
+            Assert.True(data.Count == 3);
+            Assert.True(data.ContainsKey("bar"));
+            Assert.True(data.Contains(
+                new KeyValuePair<string, object>("foo", "bar")));
+
+            var copy = new KeyValuePair<string, object>[data.Count];
+            data.CopyTo(copy, 0);
+
+            Assert.Equal(data.ToArray(), copy);
+
+            data.Clear();
+            Assert.Equal(0, data.Count);
+        }
     }
 }
