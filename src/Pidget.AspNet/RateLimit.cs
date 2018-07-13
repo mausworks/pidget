d0794b262fa9cb06ac0de56904e5ec9bb1890644
mausworks <rasmuswennerstrom@gmail.com>
2018-07-14 00:50:02 +0200

    docs: add documentation to RateLimit

--- src/Pidget.AspNet/RateLimit.cs
+++ src/Pidget.AspNet/RateLimit.cs
@@ -2,13 +2,24 @@ using System;
 
 namespace Pidget.AspNet
 {
+    /// <summary>
+    /// Maintains a state for a rate limit.
+    /// </summary>
     public class RateLimit
     {
-        private DateTimeOffset _retryAfter;
+        private DateTimeOffset _retryAfter = default;
 
+        /// <summary>
+        /// Extends the rate limit to the provided date.
+        /// </summary>
+        /// <param name="when">The date for which the rate limit expires.</param>
         public void Until(DateTimeOffset when)
             => _retryAfter = when;
 
+        /// <summary>
+        /// Whether or not the rate limit is hit at a provided date.
+        /// </summary>
+        /// <param name="at">The date to check whether the rate limit is hit at.</param>
         public bool IsHit(DateTimeOffset at)
             => at < _retryAfter;
     }
