3954a26f43192d1744076d9d396c325a0b7e8aa3
mausworks <rasmuswennerstrom@gmail.com>
2018-07-14 02:35:56 +0200

    style: Type alias long type names

--- src/Pidget.AspNet/Setup/CallbackSetup.cs
+++ src/Pidget.AspNet/Setup/CallbackSetup.cs
@@ -5,6 +5,9 @@ using Pidget.Client;
 
 namespace Pidget.AspNet.Setup
 {
+    using BeforeSendFunc = Func<SentryEventBuilder, HttpContext, Task<bool>>;
+    using AfterSendFunc = Func<SentryResponse, HttpContext, Task>;
+
     public readonly struct CallbackSetup
     {
         private readonly SentryOptions _options;
@@ -12,14 +15,14 @@ namespace Pidget.AspNet.Setup
         public CallbackSetup(SentryOptions options)
             => _options = options;
 
-        public CallbackSetup BeforeSend(Func<SentryEventBuilder, HttpContext, Task<bool>> callback)
+        public CallbackSetup BeforeSend(BeforeSendFunc callback)
         {
             _options.BeforeSendCallback = callback;
 
             return this;
         }
 
-        public CallbackSetup AfterSend(Func<SentryResponse, HttpContext, Task> callback)
+        public CallbackSetup AfterSend(AfterSendFunc callback)
         {
             _options.AfterSendCallback = callback;
 
